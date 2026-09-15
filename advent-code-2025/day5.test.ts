64ddebfb2cec39f41eeb878630352c57bf0f412d
Alison Wilbur <awilbur@forhims.com>
2026-09-15 14:08:14 -0700

    Fixed/finished day 5 part 2

--- advent-code-2025/day5.test.ts
+++ advent-code-2025/day5.test.ts
@@ -24,8 +24,12 @@ describe("day5", () => {
     expect(day5b(["3-5", "10-14", "16-20", "10-14", "12-18"])).toBe(14);
   });
 
+  it("day5b handle unsorted duplicates", () => {
+    expect(day5b(["12-13", "1-8", "1-6", "1-4", "1-5", "2-5"])).toBe(10);
+  });
+
   it("day5b handle non-ranges", () => {
-    expect(day5b(["3-3", "3-3"])).toBe(1);
+    expect(day5b(["3-3", "5-5"])).toBe(2);
   });
 
   it("day5b handle overlaps", () => {
@@ -36,16 +40,25 @@ describe("day5", () => {
     expect(day5b(["3-8", "3-5", "6-10"])).toBe(8);
   });
 
+  it("day5b handle overlap at end", () => {
+    expect(day5b(["1-3", "2-5", "7-9", "8-10"])).toBe(9);
+  });
+
+  it("day5b handle overlap at end", () => {
+    expect(day5b(["1-5", "4-8", "11-16", "12-19"])).toBe(17);
+  });
+
   it("day5b handle more overlaps", () => {
     expect(day5b(["3-5", "10-14", "16-20", "10-14", "12-18", "16-17", "3-4", "17-24"])).toBe(18);
   });
 
-  it("TODO", () => {});
-
   it("day5b from file", () => {
-    const productRanges = readFileSync(join(__dirname, "day5binput.txt"), "utf-8")
-      .trim()
-      .split("\n");
-    expect(day5b(productRanges)).toBe(341991180461521);
+    const rows = readFileSync(join(__dirname, "day5input.txt"), "utf-8").trim().split("\n");
+
+    const separatorIndex = rows.indexOf("");
+    console.log("separatorIndex", separatorIndex);
+    rows.splice(separatorIndex);
+
+    expect(day5b(rows)).toBe(352807801032167);
   });
 });
--- advent-code-2025/day5.ts
+++ advent-code-2025/day5.ts
@@ -48,97 +48,27 @@ export function day5b(rows: string[]): number {
     }
     return aEnd - bEnd;
   });
-  console.log("sorted rows", rows);
+  // console.log("sorted rows", rows);
 
-  const noOverlapRows = [];
-  for (let i = 0; i < rows.length - 1; i++) {
-    // console.log("row", rows[i]);
-    // console.log("next row", rows[i + 1]);
-    if (rows[i] === rows[i + 1]) {
-      console.log(`duplicate row, continuing: ${rows[i]}`);
-      continue;
-    }
+  let i = 0;
+  while (i < rows.length - 1) {
     const [start, end] = rows[i].split("-").map(Number);
     const [nextStart, nextEnd] = rows[i + 1].split("-").map(Number);
-    // overlap
-    if (end >= nextStart) {
-      // if they start the same, then this row is inside the next one and we can skip it
-      if (start === nextStart) {
-        console.log(`this row is inside the next one, skipping it: ${rows[i]}`);
-        continue;
-      }
-      // if this row ends after the start of the next one, we should truncate this range
-      console.log(
-        `found overlap, changing this row to ${start}-${nextStart - 1}. next row is ${rows[i + 1]}`
-      );
-      rows[i] = `${start}-${nextStart - 1}`;
+    if (end < nextStart) {
+      // console.log(`no overlap, continuing: [${rows[i]}], [${rows[i + 1]}]`);
+      i++;
+      continue;
     }
-    noOverlapRows.push(rows[i]);
+    // console.log(`merging [${rows[i]}], [${rows[i + 1]}] to [${start}-${Math.max(end, nextEnd)}]`);
+    rows[i] = `${start}-${Math.max(end, nextEnd)}`;
+    rows.splice(i + 1, 1);
   }
-  noOverlapRows.push(rows[rows.length - 1]);
-  console.log("non-overlapping rows", noOverlapRows);
+  // console.log("merged rows", rows);
 
-  for (const noRow of noOverlapRows) {
-    const [start, end] = noRow.split("-").map(Number);
+  for (const mRow of rows) {
+    const [start, end] = mRow.split("-").map(Number);
     total += end - start + 1;
   }
 
-  console.log(`total: ${total}`);
   return total;
 }
-
-// export function day5(rows: string[]): number {
-//   const foodToFreshnessMap = new Array<number>();
-//   let total = 0;
-
-//   rows.map((row) => {
-//     console.log(row);
-//     if (row === "") {
-//       return;
-//     }
-//     const [s, e] = row.split("-");
-//     const [start, end] = [Number(s), Number(e)];
-//     for (let i = start; i <= end; i++) {
-//       foodToFreshnessMap[i] = 1;
-//     }
-//   });
-
-//   console.log("foodToFreshnessMap", foodToFreshnessMap);
-
-//   rows.map((row) => {
-//     const foodID = Number(row);
-//     if (foodToFreshnessMap[foodID]) {
-//       total++;
-//     }
-//   });
-
-//   return total;
-// }
-
-//export function day5(rows: string[]): number {
-//   const foodToFreshnessMap = new Map<string, boolean>();
-//   let total = 0;
-
-//   rows.map((row) => {
-//     console.log(row);
-//     if (row === "") {
-//       return;
-//     }
-//     const [s, e] = row.split("-");
-//     const [start, end] = [Number(s), Number(e)];
-//     for (let i = start; i <= end; i++) {
-//       foodToFreshnessMap.set(`${i}`, true);
-//     }
-//   });
-
-//   console.log("foodToFreshnessMap", foodToFreshnessMap);
-
-//   rows.map((row) => {
-//     const foodID = Number(row);
-//     if (foodToFreshnessMap.has(`${foodID}`)) {
-//       total++;
-//     }
-//   });
-
-//   return total;
-// }
