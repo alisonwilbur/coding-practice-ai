17e862a181f12034ca215c2e093c02b9774f9333
Alison Wilbur <awilbur@forhims.com>
2026-09-15 13:09:39 -0700

    day 5 b WIP

--- advent-code-2025/day5.test.ts
+++ advent-code-2025/day5.test.ts
@@ -1,18 +1,51 @@
 import { readFileSync } from "fs";
 import { join } from "path";
-import { day5 } from "./day5";
+import { day5, day5b } from "./day5";
 
 // npm run test:watch advent-code-2025/day5*
 
 describe("day5", () => {
-  it("handles example", () => {
-    expect(day5(["3-5", "10-14", "16-20", "12-18", "", "1", "5", "8", "11", "17", "32"])).toBe(3);
+  // it("day5 handles example", () => {
+  //   expect(day5(["3-5", "10-14", "16-20", "12-18", "", "1", "5", "8", "11", "17", "32"])).toBe(3);
+  // });
+
+  // it("day5 from file", () => {
+  //   const productRanges = readFileSync(join(__dirname, "day5input.txt"), "utf-8")
+  //     .trim()
+  //     .split("\n");
+  //   expect(day5(productRanges)).toBe(698);
+  // });
+
+  it("day5b handles example", () => {
+    expect(day5b(["3-5", "10-14", "16-20", "12-18"])).toBe(14);
   });
 
-  it("from file", () => {
-    const productRanges = readFileSync(join(__dirname, "day5input.txt"), "utf-8")
+  it("day5b handle duplicates", () => {
+    expect(day5b(["3-5", "10-14", "16-20", "10-14", "12-18"])).toBe(14);
+  });
+
+  it("day5b handle non-ranges", () => {
+    expect(day5b(["3-3", "3-3"])).toBe(1);
+  });
+
+  it("day5b handle overlaps", () => {
+    expect(day5b(["3-8", "1-5"])).toBe(8);
+  });
+
+  it("day5b handle inside", () => {
+    expect(day5b(["3-8", "3-5", "6-10"])).toBe(8);
+  });
+
+  it("day5b handle more overlaps", () => {
+    expect(day5b(["3-5", "10-14", "16-20", "10-14", "12-18", "16-17", "3-4", "17-24"])).toBe(18);
+  });
+
+  it("TODO", () => {});
+
+  it("day5b from file", () => {
+    const productRanges = readFileSync(join(__dirname, "day5binput.txt"), "utf-8")
       .trim()
       .split("\n");
-    expect(day5(productRanges)).toBe(698);
+    expect(day5b(productRanges)).toBe(341991180461521);
   });
 });
--- advent-code-2025/day5.ts
+++ advent-code-2025/day5.ts
@@ -37,6 +37,56 @@ export function day5(rows: string[]): number {
   return total;
 }
 
+export function day5b(rows: string[]): number {
+  let total = 0;
+
+  rows.sort((a, b) => {
+    const [aStart, aEnd] = a.split("-").map(Number);
+    const [bStart, bEnd] = b.split("-").map(Number);
+    if (aStart !== bStart) {
+      return aStart - bStart;
+    }
+    return aEnd - bEnd;
+  });
+  console.log("sorted rows", rows);
+
+  const noOverlapRows = [];
+  for (let i = 0; i < rows.length - 1; i++) {
+    // console.log("row", rows[i]);
+    // console.log("next row", rows[i + 1]);
+    if (rows[i] === rows[i + 1]) {
+      console.log(`duplicate row, continuing: ${rows[i]}`);
+      continue;
+    }
+    const [start, end] = rows[i].split("-").map(Number);
+    const [nextStart, nextEnd] = rows[i + 1].split("-").map(Number);
+    // overlap
+    if (end >= nextStart) {
+      // if they start the same, then this row is inside the next one and we can skip it
+      if (start === nextStart) {
+        console.log(`this row is inside the next one, skipping it: ${rows[i]}`);
+        continue;
+      }
+      // if this row ends after the start of the next one, we should truncate this range
+      console.log(
+        `found overlap, changing this row to ${start}-${nextStart - 1}. next row is ${rows[i + 1]}`
+      );
+      rows[i] = `${start}-${nextStart - 1}`;
+    }
+    noOverlapRows.push(rows[i]);
+  }
+  noOverlapRows.push(rows[rows.length - 1]);
+  console.log("non-overlapping rows", noOverlapRows);
+
+  for (const noRow of noOverlapRows) {
+    const [start, end] = noRow.split("-").map(Number);
+    total += end - start + 1;
+  }
+
+  console.log(`total: ${total}`);
+  return total;
+}
+
 // export function day5(rows: string[]): number {
 //   const foodToFreshnessMap = new Array<number>();
 //   let total = 0;
